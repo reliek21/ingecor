1d4a8052388d379713a5bbcca29233c6f57368b9
Reliek <keilerguardo@gmail.com>
2021-08-06 12:01:41 -0500

    Agregando la clase registros

---
+++ Registros.java
@@ -0,0 +1,13 @@
+public class Registros extends Servicios {
+    private String codigoReporte;
+
+    // Getter
+    public String getCodigoReporte() {
+        return codigoReporte;
+    }
+
+    // Setter
+    public void setCodigoReporte(String newCodigoReporte) {
+        this.codigoReporte = newCodigoReporte;
+    }
+}
