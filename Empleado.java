4203e809bc1fbf98316199e7355253cdd2755737
Reliek <keilerguardo@gmail.com>
2021-08-06 12:00:11 -0500

    Agregando la clase empleado

---
+++ Empleado.java
@@ -0,0 +1,12 @@
+public class Empleado extends Personas {
+    private int cargo;
+
+    // Getter
+    public int getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(int newCargo) {
+        this.cargo = newCargo;
+    }
+}
