374359c930c04b0c88bcd91c6926d2e2ec5bb1da
Reliek <keilerguardo@gmail.com>
2021-08-06 11:53:56 -0500

    Creando la clase de categoria

---
+++ Categoria.java
@@ -0,0 +1,22 @@
+public class Categoria {
+    private int id;
+    private String nombre;
+
+    // Getter
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    // Setter
+    public void setid(int newId) {
+        this.id = newId;
+    }
+
+    public void setNombre(String newNombre) {
+        this.nombre = newNombre;
+    }
+}
